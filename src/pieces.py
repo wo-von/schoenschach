51a378540b2c65091826f8311c7313b65e70c63e
sina hosseinizad <24656395+wo-von@users.noreply.github.com>
2021-11-11 16:23:41 +0100

    Day6: black

--- src/pieces.py
+++ src/pieces.py
@@ -54,80 +54,189 @@ class Rook(Piece):
     def allowed_moves(self):
         raise NotImplementedError
 
+
 class WRook(Rook):
     logical, color = "wr", "w"
+
     def __init__(self, x, y):
-       Rook.__init__(self, x = x, y = y, image = symbolsPath[current_theme][self.logical], logical = WRook.logical)
+        Rook.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=WRook.logical,
+        )
+
 
 class BRook(Rook):
-   logical, color = "br", "b"
-   def __init__(self, x, y):
-       Rook.__init__(self, x = x, y = y, image = symbolsPath[current_theme][self.logical], logical = BRook.logical) 
+    logical, color = "br", "b"
+
+    def __init__(self, x, y):
+        Rook.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=BRook.logical,
+        )
+
+
 class Knight(Piece):
     def allowed_moves(self):
         raise NotImplementedError
 
+
 class WKnight(Knight):
     logical, color = "wn", "w"
+
     def __init__(self, x, y):
-        Knight.__init__(self, x = x, y = y, image=symbolsPath[current_theme][self.logical], logical= WKnight.logical)
+        Knight.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=WKnight.logical,
+        )
+
 
 class BKnight(Knight):
     logical, color = "bn", "b"
+
     def __init__(self, x, y):
-        Knight.__init__(self, x = x, y=y, image=symbolsPath[current_theme][self.logical], logical = BKnight.logical)
+        Knight.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=BKnight.logical,
+        )
+
 
 class Bishop(Piece):
     def allowed_moves(self):
         raise NotImplementedError
 
+
 class WBishop(Bishop):
     logical, color = "wb", "w"
+
     def __init__(self, x, y):
-        Bishop.__init__(self, x = x, y = y, image=symbolsPath[current_theme][self.logical], logical=WBishop.logical)
+        Bishop.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=WBishop.logical,
+        )
+
 
 class BBishop(Bishop):
     logical, color = "bb", "b"
+
     def __init__(self, x, y):
-        Bishop.__init__(self, x = x, y = y, image=symbolsPath[current_theme][self.logical], logical = BBishop.logical)
+        Bishop.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=BBishop.logical,
+        )
+
 
 class Queen(Piece):
     def allowed_moves(self):
         raise NotImplementedError
 
+
 class WQueen(Queen):
     logical, color = "wq", "w"
+
     def __init__(self, x, y):
-        Queen.__init__(self, x = x, y = y, image=symbolsPath[current_theme][self.logical], logical = WQueen.logical)
+        Queen.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=WQueen.logical,
+        )
+
 
 class BQueen(Queen):
     logical, color = "bq", "b"
+
     def __init__(self, x, y):
-        Queen.__init__(self, x = x, y = y, image=symbolsPath[current_theme][self.logical], logical = BQueen.logical)
+        Queen.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=BQueen.logical,
+        )
+
 
 class King(Piece):
     def allowed_moves(self):
         raise NotImplementedError
+
     def is_checked(self):
         raise NotImplementedError
+
     def is_mate(self):
         raise NotImplementedError
+
+
 class WKing(King):
     logical, color = "wk", "w"
+
     def __init__(self, x, y):
-        King.__init__(self, x = x, y=y, image=symbolsPath[current_theme][self.logical], logical=WKing.logical)
+        King.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=WKing.logical,
+        )
+
+
 class BKing(King):
     logical, color = "bk", "b"
+
     def __init__(self, x, y):
-        King.__init__(self, x = x, y = y, image=symbolsPath[current_theme][self.logical], logical=BKing.logical)
+        King.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=BKing.logical,
+        )
+
+
 class Pawn(Piece):
     def allowed_moves(self):
         raise NotImplementedError
+
+
 class WPawn(Pawn):
     logical, color = "wp", "w"
+
     def __init__(self, x, y):
-        Pawn.__init__(self, x = x, y = y, image=symbolsPath[current_theme][self.logical] ,logical=WPawn.logical)
+        Pawn.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=WPawn.logical,
+        )
+
+
 class BPawn(Pawn):
-    logical , color = "bp","b"
+    logical, color = "bp", "b"
+
     def __init__(self, x, y):
-        Pawn.__init__(self, x = x, y=y, image=symbolsPath[current_theme][self.logical], logical=BPawn.logical)
+        Pawn.__init__(
+            self,
+            x=x,
+            y=y,
+            image=symbolsPath[current_theme][self.logical],
+            logical=BPawn.logical,
+        )
